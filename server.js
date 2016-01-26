0bd758cefe0f9ae61d66be93609f5839b1e9784f
Joe Lepper <jlepper@newrelic.com>
2016-01-26 09:47:11 -0800

    get prod in shape for some SSL action

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var webpackDevMiddleware = require('webpack-dev-middleware')
 var config = require('./webpack.config')
 var React = require('react')
 var ReactDOM = require('react-dom/server')
+var port = 3000
 
 var Page = require('./src/page').Page
 
 var __PROD__ = process.env.NODE_ENV === 'production'
-var __DEV__ = !__PROD__
+var __STAGING__ = process.env.NODE_ENV === 'staging'
+var __DEV__ = !__PROD__ && !__STAGING__
 var __COLS__ = Math.min(process.env.COLS, 128)
 var __ROWS__ = Math.min(process.env.ROWS, 72)
 
@@ -19,7 +21,10 @@ if (__DEV__) {
   var compiler = webpack(config)
   app.use(webpackDevMiddleware(compiler, { stats: { colors: true } }))
 }
-else app.use(express.static('dist'))
+else {
+  if (__PROD__) port = 80
+  app.use(express.static('dist'))
+}
 
 app.get('/', (req, res) => {
   var cols = Math.min(req.query.cols || __COLS__, 128)
